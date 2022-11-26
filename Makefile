40e73ec853b63edbc2e3bfbec90db36c782710d5
anguuan <anguuan@outlook.com>
2022-11-26 12:21:58 +0800

    add unit tests for lexer

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 .PHONY: all
-all: lexer parser ;
+all: lexer parser unittest;
 
 .PHONY: lexer
 lexer: build
@@ -8,6 +8,10 @@ lexer: build
 .PHONY: parser
 parser: build
 	cmake --build build --target parser
+
+.PHONY: unittest
+unittest: build
+	cmake --build build --target main_test
 
 build: CMakeLists.txt
 	cmake -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -Bbuild
@@ -21,7 +25,7 @@ run-parser: parser
 	./build/parser
 
 .PHONY: test
-test: lexer parser
+test: lexer parser unittest
 	cd build && ctest -j18 -C Debug -T test --output-on-failure
 
 .PHONY: clean
--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,9 +1,36 @@
+#include "lexer.h"
+#include "parser.h"
 #include <cstdio>
+#include <fstream>
 #include <gtest/gtest.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-TEST(MainTest, HelloWorld)
+void test_lexer(const string& name)
 {
-    EXPECT_TRUE(1 == 1);
+    string program_file_path = string(WORKING_DIR) + "/programs/" + name + ".bzl";
+    FILE* fp = fopen(program_file_path.c_str(), "r");
+    EXPECT_TRUE(fp != nullptr);
+    yyin = fp;
+    string expected_output_path = string(WORKING_DIR) + "/expected_lexer_output/" + name + ".txt";
+    ifstream ifs(expected_output_path);
+    EXPECT_TRUE(ifs.good());
+    string expected_output((istreambuf_iterator<char>(ifs)), (istreambuf_iterator<char>()));
+    string output;
+    YYSTYPE yylval;
+    for (;;) {
+        int token = yylex(&yylval);
+        if (!token) {
+            break;
+        }
+        output += token_to_str(token, yylval) + '\n';
+    }
+    EXPECT_TRUE(output == expected_output) << output;
+}
+
+TEST(MainTest, TestLexer1)
+{
+    test_lexer("1");
 }
