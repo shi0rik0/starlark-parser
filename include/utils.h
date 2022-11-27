277f168695a36d359793e746f8d087f0467e9c3e
anguuan <anguuan@outlook.com>
2022-11-27 15:02:12 +0800

    add a utility macro FATAL_ERROR(msg)

--- include/utils.h
+++ include/utils.h
@@ -11,4 +11,10 @@ std::string token_to_str(int token, const YYSTYPE& value);
 // the type of @format cannot be std::string
 std::string sprintfpp(const char* format, ...);
 
+// @msg should be a std::string
+#define FATAL_ERROR(msg)                                                             \
+    do {                                                                             \
+        fatal_error(sprintfpp("error: %s:%d: %s", __FILE__, __LINE__, msg.c_str())); \
+    } while (false)
+
 #endif // UTILS_H
