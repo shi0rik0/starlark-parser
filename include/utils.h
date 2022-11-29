4e3528a2a7d269fa39434a377b4e888307793ed6
anguuan <anguuan@outlook.com>
2022-11-29 16:07:06 +0800

    add a helper macro ASSERT(p)

--- include/utils.h
+++ include/utils.h
@@ -11,10 +11,23 @@ std::string token_to_str(int token, const YYSTYPE& value);
 // the type of @format cannot be std::string
 std::string sprintfpp(const char* format, ...);
 
-// @msg should be a std::string
-#define FATAL_ERROR(msg)                                                             \
-    do {                                                                             \
-        fatal_error(sprintfpp("error: %s:%d: %s", __FILE__, __LINE__, msg.c_str())); \
+// @msg can be std::string or const char*
+#define FATAL_ERROR(msg)                                                      \
+    do {                                                                      \
+        fatal_error(sprintfpp("error: %s:%d: ", __FILE__, __LINE__) + (msg)); \
     } while (false)
+
+#ifdef DEBUG
+#define ASSERT(p)                                 \
+    do {                                          \
+        if (!(p)) {                               \
+            FATAL_ERROR("assertion failed: " #p); \
+        }                                         \
+    } while (false)
+#else
+#define ASSERT(p) \
+    do {          \
+    } while (false)
+#endif
 
 #endif // UTILS_H
